b2a1e18442ea5cd6d86c6b12769f39b9f895a6cb
Glitch (ports-and-connections-match) <none>
2018-03-20 15:21:51 +0000

    🚎🚍 Updated with Glitch 3.20

--- script.js
+++ script.js
@@ -1,100 +1,102 @@
+// Ports and Connections Game
+
 //make array of objects
 var img_array = [
   {
-    type: "port", // port/plug only
+    type: "port",
     name: "USB-A",
-    image: "",
+    image: "https://upload.wikimedia.org/wikipedia/commons/5/5a/USB_Type-C_plug_20170626.jpg",
     credit: "Mk2010",
     creditLink: "https://commons.wikimedia.org/wiki/User:Mk2010"
   },
   {
     type: "plug",
     name: "USB-A",
-    image: "",
+    image: "https://upload.wikimedia.org/wikipedia/commons/5/5a/USB_Type-C_plug_20170626.jpg",
     credit: "Marco Verch",
     creditLink: "https://www.flickr.com/photos/30478819@N08/"
   },
   {
     type: "port",
     name: "HDMI",
-    image: "",
+    image: "https://upload.wikimedia.org/wikipedia/commons/5/5a/USB_Type-C_plug_20170626.jpg",
     credit: "Max Pixel",
     creditLink: "http://maxpixel.freegreatpicture.com/static/photo/1x/Usb-Vga-Hdmi-1394-Laptop-Lan-1884.jpg"    
   },
   {
     type: "plug",
     name: "HDMI",
-    image: "",
+    image: "https://upload.wikimedia.org/wikipedia/commons/5/5a/USB_Type-C_plug_20170626.jpg",
     credit: "D-Kuru",
     creditLink: "https://commons.wikimedia.org/wiki/User:D-Kuru"    
   },
   {
     type: "port",
     name: "VGA",
-    image: "",
+    image: "https://upload.wikimedia.org/wikipedia/commons/5/5a/USB_Type-C_plug_20170626.jpg",
     credit: "Duncan Lithgow",
     creditLink: "https://en.wikipedia.org/wiki/User:DuLithgow"   
   },
   {
     type: "plug",
     name: "VGA",
-    image: "",
+    image: "https://upload.wikimedia.org/wikipedia/commons/5/5a/USB_Type-C_plug_20170626.jpg",
     credit: "Evan-Amos",
     creditLink: "https://commons.wikimedia.org/wiki/User:Evan-Amos"  
   },
   {
     type: "port",
     name: "Audio3.5",
-    image: "",
+    image: "https://upload.wikimedia.org/wikipedia/commons/5/5a/USB_Type-C_plug_20170626.jpg",
     credit: "Bubba73 (Jud McCranie)",
     creditLink: "https://commons.wikimedia.org/wiki/User:Bubba73"    
   },
   {
     type: "plug",
     name: "Audio3.5",
-    image: "",
+    image: "https://upload.wikimedia.org/wikipedia/commons/5/5a/USB_Type-C_plug_20170626.jpg",
     credit: "an3k",
     creditLink: "https://commons.wikimedia.org/wiki/File:Klinkenstecker_5-polig.jpg"    
   },
   {
     type: "port",
     name: "SD",
-    image: "",
+    image: "https://upload.wikimedia.org/wikipedia/commons/5/5a/USB_Type-C_plug_20170626.jpg",
     credit: "seagul",
     creditLink: "https://pixabay.com/en/usb-port-sd-card-laptop-sound-829329/"    
   },
   {
     type: "plug",
     name: "SD",
-    image: "",
+    image: "https://upload.wikimedia.org/wikipedia/commons/5/5a/USB_Type-C_plug_20170626.jpg",
     credit: "Zxb",
     creditLink: "https://commons.wikimedia.org/wiki/File:Flash_memory_cards_size.jpg"   
   },
   {
     type: "port",
     name: "Ethernet",
-    image: "",
+    image: "https://upload.wikimedia.org/wikipedia/commons/5/5a/USB_Type-C_plug_20170626.jpg",
     credit: "Asim18",
     creditLink: "https://upload.wikimedia.org/wikipedia/commons/2/21/Adsl_connections.jpg" 
   },
   {
     type: "plug",
     name: "Ethernet",
-    image: "",
+    image: "https://upload.wikimedia.org/wikipedia/commons/5/5a/USB_Type-C_plug_20170626.jpg",
     credit: "David Monniaux",
     creditLink: "https://commons.wikimedia.org/wiki/Ethernet#/media/File:Ethernet_RJ45_connector_p1160054.jpg"   
   },
   {
     type: "port",
     name: "Thunderbolt/Mini Display Port",
-    image: "",
+    image: "https://upload.wikimedia.org/wikipedia/commons/5/5a/USB_Type-C_plug_20170626.jpg",
     credit: "Aurélien Selle",
     creditLink: "https://commons.wikimedia.org/wiki/File:Mini_DisplayPort_on_Apple_MacBook.jpg"   
   },
   {
     type: "plug",
     name: "Thunderbolt/Mini Display Port",
-    image: "",
+    image: "https://upload.wikimedia.org/wikipedia/commons/5/5a/USB_Type-C_plug_20170626.jpg",
     credit: "Palthrow",
     creditLink: "https://cs.wikipedia.org/wiki/Mini_DisplayPort#/media/File:Mini_displayport.jpg"    
   },
@@ -145,7 +147,8 @@ buildBoard();
 // 2. Listen for players clicking on cards
 $( ".card" ).click( function() {
   console.log("You clicked!");
-  checkClick($(this));
+  $(this).find(".card-image").show(0);
+  //checkClick($(this));
 });
 
 // 3. Check how many cards player has shown, check card state of clicked card
@@ -167,18 +170,17 @@ function checkClick(card){
     }
   });
   
-  if (cardsShown.length < 2) {
-    //$(this).find(".card-name").slideUp();
-    $(card).find(".card-name").hide();
-    //$(this).find(".card-image").show( "slow" );
+  if (cardsShown.length <= 2) {
     $(card).find(".card-image").show();
+    $(card).find(".card-name").hide();
     $(card).attr("state", "shown");
     cardsShown.push($(card));
-  }
+  } 
   
-  if (cardsShown.length === 2) {
-   checkMatch(cardsShown[0], cardsShown[1]);
-  }
+  setTimeout(function checking(){
+   if (cardsShown.length === 2) {
+    checkMatch(cardsShown[0], cardsShown[1]);
+  }}, 550);
   
   // Is clicking a third card a problem?
 }
@@ -196,18 +198,16 @@ function checkMatch(card1, card2) {
 // 4A. Cards are matched -> 6
 // 4b. Cards are not matched -> 5
 
-
 // 5. Unshow cards -> 1.5
 function unshow(card1, card2){
   console.log("unshow()");
-  card1.find("card-image").hide();
-  card1.find("card-name").show();
+  card1.find(".card-name").show();
+  card1.find(".card-image").hide();
   card1.attr("state", "unshown");
-  card2.find("card-image").hide();
-  card2.find("card-name").show();
+  card2.find(".card-name").show();
+  card2.find(".card-image").hide(); 
   card2.attr("state", "unshown");
-}
-
+  }
 // 6.  Keep cards shown, change card state to matched 
 function show( card1, card2){
   console.log("show()");
@@ -229,15 +229,12 @@ function checkGame() {
     }
   });
   
-  if (gameOver) {
+  if (gameOver) { // 7B: All are matched -> 8
     youWin();
-  } else {
+  } else { // 7A: Not all are matched -> 1.5
     return
   } 
 }
-// 7A: Not all are matched -> 1.5
-
-// 7B: All are matched -> 8
 
 // 8. You win! message -> 9
 // thoughts: mouse create submission
@@ -250,57 +247,3 @@ function youWin() {
 
 // 9B: NO -> nothing
 
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-var count = 0;
-function on(){
-  while ( count <= img_array.length()) {
-  $( ".card" ).click( function() { 
-    $(this).slideUp()
-    $(".card-image").show( "slow" )
-//    click(this);
-  });
-
-    /*
-    function click( a ){ $( ".card" ).click(function() {isMatch(a, this)});};
-  function isMatch( t, g ) { 
-    if($( t ).find( ".matchname" ).text() == $( g ).find( ".matchname" ).text() && $( t ).find( ".matchtype" ).text() != $( g ).find( ".matchtype" ).text()){
-      $( t ).slideUp();
-      $( g ).slideUp();
-      count++;
-    }
-    else{
-      count --; //
-    }
-  }
-  }
-  
-  
-}
-
-on();
-                                                                    
-*/
-
-//define function isMatch()
-
-  //results: wrong
-
-  //results: right
-
-//Count # of right/wrong
-
-//winning
-
-//shuffle?
